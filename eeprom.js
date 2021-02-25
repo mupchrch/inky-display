f23e3278df0f03aafbacfdb24c62f527752349cd
Mike Upchurch <mupchrch@users.noreply.github.com>
2021-02-25 22:03:39 +0000

    initial

---
+++ eeprom.js
@@ -0,0 +1,23 @@
+const i2c = require('i2c-bus');
+const struct = require('python-struct');
+
+const EEP_ADDRESS = 0x50;
+const I2C_BLOCK = [0x00];
+const BLOCK_READ_LENGTH = 29;
+
+async function read(i2cBus = 1) {
+  const openI2c = await i2c.openPromisified(i2cBus);
+  const { bytesWritten } = await openI2c.writeI2cBlock(EEP_ADDRESS, 0, I2C_BLOCK.length, Buffer.from(I2C_BLOCK));
+
+  console.log(`${bytesWritten} BYTE WRITTEN`);
+
+  const { bytesRead, buffer } = await openI2c.readI2cBlock(EEP_ADDRESS, 0, BLOCK_READ_LENGTH, Buffer.alloc(BLOCK_READ_LENGTH));
+
+  console.log(`${bytesRead} BYTES READ`);
+
+  const [width, height, color, pcbVariant, displayVariant, writeTime] = struct.unpack('<HHBBB22p', buffer);
+
+  return { width, height, color, pcbVariant, displayVariant, writeTime };
+}
+
+module.exports = { read };
